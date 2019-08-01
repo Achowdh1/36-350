47846adf9db1a3c7f74385735a547fe1f2b76f58
Achowdh1 <49734992+Achowdh1@users.noreply.github.com>
2019-08-01 12:57:54 -0500

    model_fit function

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,4 +4,14 @@ generate_data = function(n, p){
   return(list(covariates = cov, responses = responses))
 } 
 
-
+model_fit = function(covariates, responses, cutoff){
+  lm_1 = lm(responses ~ covariates)
+  inds = (summary(lm_1)$coefficients)[-1,"Pr(>|t|)"] < cutoff
+  print(summary(lm_1))
+  print(inds)
+  if(all(inds == FALSE)){return(c())}
+  lm_2 = lm(responses~covariates[,inds])
+  return(lm_2)
+}
+a = generate_data(10,5)
+model_fit(a$covariates, a$responses, 0.7)
