0831e4a2aeb902e5f34e23029957aedefb813685
Achowdh1 <49734992+Achowdh1@users.noreply.github.com>
2019-08-01 13:15:40 -0500

    run simulation function added

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -7,11 +7,33 @@ generate_data = function(n, p){
 model_fit = function(covariates, responses, cutoff){
   lm_1 = lm(responses ~ covariates)
   inds = (summary(lm_1)$coefficients)[-1,"Pr(>|t|)"] < cutoff
-  print(summary(lm_1))
-  print(inds)
   if(all(inds == FALSE)){return(c())}
   lm_2 = lm(responses~covariates[,inds])
   return(lm_2)
 }
-a = generate_data(10,5)
-model_fit(a$covariates, a$responses, 0.7)
+
+library(ggplot2)
+run_simulation = function(n_trials, n, p, cutoff){
+  results = c()
+  for(i in 1:n_trials){
+    rnorm_data = generate_data(n, p)
+    lm_fit = model_fit(rnorm_data$covariates, rnorm_data$responses, cutoff)
+    if(!is.null(lm_fit)){
+    pvals = (summary(lm_fit)$coefficients)[-1,"Pr(>|t|)"]
+    results = c(results, pvals) }
+  }
+  p = ggplot(data.frame(pvals = results)) + geom_histogram(aes(x = pvals, y = ..density..))
+  print(p)
+}
+
+
+#run_simulation(1000, 100, 10, 0.1)
+
+#n <- c(100, 1000, 10000) ; p <- c(10, 20, 50)
+
+#figure code, commented out
+#for(nval in n){
+#  for(pval in p){
+#    run_simulation(1000, nval, pval, 0.05)
+#  }
+#}
